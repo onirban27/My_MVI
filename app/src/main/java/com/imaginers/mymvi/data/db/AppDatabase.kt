3be9e68e0a7e8a3c0123a6734f583fe332daf9cc
M Onirban <onirban27@gmail.com>
2021-07-15 01:36:26 +0600

    export schema key value added.

--- app/src/main/java/com/imaginers/mymvi/data/db/AppDatabase.kt
+++ app/src/main/java/com/imaginers/mymvi/data/db/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import com.imaginers.mymvi.data.db.entity.User
 
 
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class], version = 1 , exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
